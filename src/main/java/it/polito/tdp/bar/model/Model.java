628c148efbd10bf8e27f4a9160778510408ab021
Marco Camaggio <100542819+mcamaggio@users.noreply.github.com>
2022-05-18 19:13:03 +0200

    2022/05/18

--- src/main/java/it/polito/tdp/bar/model/Model.java
+++ src/main/java/it/polito/tdp/bar/model/Model.java
@@ -1,5 +1,16 @@
 package it.polito.tdp.bar.model;
 
 public class Model {
+	private Simulator sim;
+	
+	public Model() {
+		sim = new Simulator();
+	}
+	
+	public Statistiche simula() {
+		sim.init();
+		sim.run();
+		return sim.getStatistiche();
+	}
 
 }
